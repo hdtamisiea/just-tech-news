e5b9e36675a9a9492ba76f615d47049c9ccc544e
htam <hdfair@gmail.com>
2022-03-30 11:31:38 -0400

    added login.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -33,7 +33,6 @@ router.get("/", (req, res) => {
   })
     .then((dbPostData) => {
       // pass a single post object into the homepage template
-      console.log(dbPostData[0]);
       const posts = dbPostData.map(post => post.get({ plain: true }));
       res.render('homepage', { posts });
     })
@@ -42,5 +41,9 @@ router.get("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.get('/login', (req, res) => {
+    res.render('login');
+  });
 
 module.exports = router;
